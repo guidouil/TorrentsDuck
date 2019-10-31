72559a6b05f07f66977e5cf21cdad165c29de386
Guidouil <guidouil@gmail.com>
2019-10-31 20:40:10 +0100

    Fix ftp

--- ftp/server/ftpServer.js
+++ ftp/server/ftpServer.js
@@ -5,20 +5,23 @@ import FtpSrv from 'ftp-srv';
 Meteor.startup(() => {
   const ftpServer = new FtpSrv({
     url: `ftp://0.0.0.0:${Meteor.settings.public.ftpPort}`,
-    // pasv_url: 'ftp://0.0.0.0',
+    pasv_url: Meteor.settings.serverIp,
     pasv_min: Meteor.settings.ftpPasvMin,
     pasv_max: Meteor.settings.ftpPasvMax,
     greeting: 'Welcome to Torrents Duck FTP server',
-    // timeout: 60000,
+    tls: false,
+    anonymous: false,
+    file_format: 'ls',
+    timeout: 60000,
   });
 
   async function isValidUser(username, password) {
-    const user = await Accounts.findUserByUsername(username);
+    const user = Accounts.findUserByUsername(username);
     if (!user || !(user.isValid || user.isAdmin)) {
       return false;
     }
     // eslint-disable-next-line no-underscore-dangle
-    const result = await Accounts._checkPassword(user, password);
+    const result = Accounts._checkPassword(user, password);
     if (result.error) {
       return false;
     }
@@ -28,7 +31,7 @@ Meteor.startup(() => {
   ftpServer.on('login', ({ username, password }, resolve, reject) => {
     isValidUser(username, password).then((result) => {
       if (result) {
-        resolve({ root: Meteor.settings.torrentsPath });
+        resolve({ root: Meteor.settings.torrentsPath.slice(0, -1) });
       } else {
         reject(new Error('Invalid username or password...'));
       }
